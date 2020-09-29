02a9458aed07d3627c93caac69b1389ca92a3e1c
Christopher Collins <chris@ccollins.io>
2020-09-28 23:48:00 -0600

    Revert "removed console.log"
    This reverts commit 07e5b35ff64da89b962834e25d783f7fafe297af.

--- usecontext-basic/src/components/Counter.js
+++ usecontext-basic/src/components/Counter.js
@@ -3,6 +3,7 @@ import { BasicContext } from "../utilities/BasicContext";
 
 export function Counter() {
   const [, setCount] = React.useContext(BasicContext);
+  console.log(setCount);
   const increment = () => setCount((c) => c + 1);
   return <button onClick={increment}>Click Me to Increase</button>;
 }
