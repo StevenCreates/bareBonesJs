edd5f903a90b6162f8c84226267e820cd8204ac7
Steven Hofheins <stevenisinsane@gmail.com>
2020-10-09 17:14:12 -0600

    added components into appjs

--- react-firestore/src/App.js
+++ react-firestore/src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Add from "./components/Add";
+import Edit from "./components/Edit";
 
 function App() {
-  return <div className='App'>Basic FireStore App</div>;
+  return (
+    <div>
+      <span>Basic FireStore App</span>
+      <Add />
+      <Edit />
+      <Remove />
+    </div>
+  );
 }
 
 export default App;
