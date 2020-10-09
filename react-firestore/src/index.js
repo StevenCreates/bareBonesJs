2b46b00fea3f1c38a459ac6625a01226602360f8
Steven Hofheins <stevenisinsane@gmail.com>
2020-10-09 17:54:11 -0600

    added firebase initializer to index.js

--- react-firestore/src/index.js
+++ react-firestore/src/index.js
@@ -2,6 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import firebase from "firebase";
+
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  databaseURL: process.env.REACT_APP_databaseURL,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+});
 
 ReactDOM.render(
   <React.StrictMode>
